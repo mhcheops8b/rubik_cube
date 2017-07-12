b45021b1c85279d138fe2ff8f767044878cf546a
Marek Hycko <mhycko@gmail.com>
2017-07-12 09:20:21 +0200

    Adding other moves into cube333 struct in test_cube.cpp.

--- test_cube.cpp
+++ test_cube.cpp
@@ -8,9 +8,22 @@
 struct cube333 {
 	rubik_cube::Corners c;
 	rubik_cube::Fixed_edges fe;
+	rubik_cube::Center_corners<3> cc;
 
 	void disp_cube(std::ostream &os = std::cout);
 	void apply_L();
+	void apply_R();
+	void apply_F();
+	void apply_B();
+	void apply_U();
+	void apply_D();
+
+	void apply_ML();
+	void apply_MR();
+	void apply_MF();
+	void apply_MB();
+	void apply_MU();
+	void apply_MD();
 };
 
 void cube333::disp_cube(std::ostream &os) {
@@ -18,11 +31,63 @@ void cube333::disp_cube(std::ostream &os) {
 	c.disp_cube();
 	os << "Edges:\n";
 	fe.disp_cube();
+	os << "Centers:\n";
+	cc.disp_cube();
 }
 
 void cube333::apply_L() {
 	c.apply_L();
 	fe.apply_L();
+}
+void cube333::apply_R() {
+	c.apply_R();
+	fe.apply_R();
+}
+void cube333::apply_F() {
+	c.apply_F();
+	fe.apply_F();
+}
+void cube333::apply_B() {
+	c.apply_B();
+	fe.apply_B();
+}
+void cube333::apply_U() {
+	c.apply_U();
+	fe.apply_U();
+}
+void cube333::apply_D() {
+	c.apply_D();
+	fe.apply_D();
+}
+
+void cube333::apply_ML() {
+	fe.apply_ML();
+	cc.apply_ML<0>();
+}
+
+void cube333::apply_MR() {
+	fe.apply_MR();
+	cc.apply_MR<0>();
+}
+
+void cube333::apply_MF() {
+	fe.apply_MF();
+	cc.apply_MF<0>();
+}
+
+void cube333::apply_MB() {
+	fe.apply_MB();
+	cc.apply_MB<0>();
+}
+
+void cube333::apply_MU() {
+	fe.apply_MU();
+	cc.apply_MU<0>();
+}
+
+void cube333::apply_MD() {
+	fe.apply_MD();
+	cc.apply_MD<0>();
 }
 
 
@@ -50,7 +115,7 @@ int main() {
 	stdcube.disp_cube();
 	std::cout << "------------\n";
 
-	stdcube.apply_L();
+	stdcube.apply_ML();
 	stdcube.c.disp();
 	std::cout << "------------\n";
 	stdcube.disp_cube();
