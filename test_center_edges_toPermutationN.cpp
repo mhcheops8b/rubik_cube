0a4c1e7814792e77b40612f97cf11077aaec8f84
Marek Hycko (at home) <mhycko@gmail.com>
2017-07-15 08:51:15 +0200

    Modifying test_center_edges_toPermutationN.cpp to contain both odd and even cases.
    Adding Visual Studion 2017 Community build.

--- test_center_edges_toPermutationN.cpp
+++ test_center_edges_toPermutationN.cpp
@@ -5,26 +5,28 @@ int main() {
 
 	rubik_cube::Center_edges<6> ce;
 	ce.toPermutationN();
-
+	std::cout << "----------------\n";
 	ce.apply_ML<1>();
+	std::cout << "----------------\n";
 	ce.toPermutationN();
-
+	std::cout << "----------------\n";
 	Permutation<6> cube;
 	cube = ce.toPermutationN(cube);
 	cube.disp_cube();
-#if 0
-	rubik_cube::Center_edges<5> ce;
 
-//	ce.toPermutationN();
 
-	ce.apply_ML<0>();
-//	ce.toPermutationN();
-	ce.disp_cube();
+	rubik_cube::Center_edges<5> ce5;
+
+	ce5.toPermutationN();
 	std::cout << "----------------\n";
-	Permutation<5> cube;
-	cube = ce.toPermutationN(cube);
-	cube.disp_cube();
-#endif
+	ce5.apply_ML<0>();
+	ce5.toPermutationN();
+	std::cout << "----------------\n";
+	ce5.disp_cube();
+	std::cout << "----------------\n";
+	Permutation<5> cube5;
+	cube5 = ce5.toPermutationN(cube5);
+	cube5.disp_cube();
 
 	return 0;
 }
